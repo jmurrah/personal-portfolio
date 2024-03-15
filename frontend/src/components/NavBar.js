fd3bc8ac50693d5706abb320d0628bc3df147f28
jmurrah <jacobhmurrah@gmail.com>
2024-03-15 07:01:27 -0500

    tailwindcss setup and working with react!

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function NavBar() {
     return (
         <div>
-            <h1>NavBar</h1>
+            <h1 className='text-yellow-500'>test</h1>
             <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
         </div>
