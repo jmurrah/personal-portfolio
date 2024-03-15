f44b2855db65347a5801d33aedc4c2f5fcad726d
jmurrah <jacobhmurrah@gmail.com>
2024-03-15 18:05:35 -0500

    switched from npm to yarn for package dependencies

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function NavBar() {
     return (
         <div>
-            <h1 className='text-yellow-500'>test</h1>
+            <h1 className='text-pink-500 text-center text-8xl'>test</h1>
             <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
         </div>
