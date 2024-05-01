549b190ca100f98934d42bccd3263c8f87a91413
jmurrah <jacobhmurrah@gmail.com>
2024-04-30 21:26:12 -0500

    unworking

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import { parseForESLint } from "@babel/eslint-parser";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+  {
+    files: ["*.js", "*.jsx"],
+    languageOptions: {
+      parser: {
+        parseForESLint,
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {},
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["craco.config.js", "tailwind.config.js"],
+    languageOptions: {
+      parser: {
+        parseForESLint,
+        requireConfigFile: false,
+      },
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {},
+  },
 ];
