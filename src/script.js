0f971d8f3fa2a735c217e9a13e1601fe425b0e8a
jmurrah <jacobhmurrah@gmail.com>
2024-06-11 14:13:50 -0500

    Finish work experience js

--- src/script.js
+++ src/script.js
@@ -1,18 +1,18 @@
 window.onload = () => {
-    document.querySelectorAll("#experience button").forEach(button => {
-        button.addEventListener("click", () => {
-            const buttonName = button.textContent.trim().toLowerCase();
-            const descriptions = document.querySelectorAll("#experience div[id]");
-            console.log(buttonName)
-            console.log(descriptions);
-            
-            descriptions.forEach(description => {
-                if (description.id !== buttonName) {
-                    description.style.display = "none";
-                } else {
-                    description.style.display = "block";
-                }
+    const buttons = document.querySelectorAll("#experience button");
+    const descriptions = document.querySelectorAll("#experience div[id]");
+
+    document.querySelector("#experience").addEventListener("click", (event) => {
+        if (event.target.tagName.toLowerCase() === 'button') {
+            const buttonName = event.target.textContent.trim().toLowerCase();
+
+            buttons.forEach(button => {
+                button.classList.remove("bg-red-600");
             });
-        });
+            descriptions.forEach(description => description.style.display = "none");
+
+            document.getElementById(buttonName).style.display = "block";
+            event.target.classList.add("bg-red-600");
+        }
     });
 }
