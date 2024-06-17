eb493d75eb49f134e4ee0d4e4ab35451f5cf8af2
jmurrah <jacobhmurrah@gmail.com>
2024-06-17 17:54:55 -0500

    Add font and being styling

--- src/script.js
+++ src/script.js
@@ -1,18 +1,28 @@
-window.onload = () => { 
+function scrollToTop () {
+    window.scrollTo(0, 0);
+    history.replaceState(null, null, window.location.pathname);
+}
+
+
+function changeDisplayedExperience(event) {
     const buttons = document.querySelectorAll("#experience button");
     const descriptions = document.querySelectorAll("#experience div[id]");
 
-    document.querySelector("#experience").addEventListener("click", (event) => {
-        if (event.target.tagName.toLowerCase() === 'button') {
-            const buttonName = event.target.textContent.trim().toLowerCase();
+    if (event.target.tagName.toLowerCase() === 'button') {
+        const buttonName = event.target.textContent.trim().toLowerCase();
 
-            buttons.forEach(button => {
-                button.classList.remove("bg-red-600");
-            });
-            descriptions.forEach(description => description.style.display = "none");
+        buttons.forEach(button => {
+            button.classList.remove("bg-red-600");
+        });
+        descriptions.forEach(description => description.style.display = "none");
 
-            document.getElementById(buttonName).style.display = "block";
-            event.target.classList.add("bg-red-600");
-        }
-    });
+        document.getElementById(buttonName).style.display = "block";
+        event.target.classList.add("bg-red-600");
+    }
+}
+
+
+window.onload = () => { 
+    document.querySelector("#experience").addEventListener("click", (event) => changeDisplayedExperience(event));
+    document.querySelector("#logo").addEventListener("click", () => scrollToTop());
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-background': '#0a0a0a',
+        'primary-purple': '#a35bff',
+        'secondary-gray': '#b5b5b5',
+        // 'accent-purple': '#9000ff'
+      }
+    },
   },
   plugins: [],
 }
