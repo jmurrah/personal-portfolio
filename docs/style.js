15d7a7bd8aceb72d1ccffb055ba89ff6c3546110
jmurrah <jacobhmurrah@gmail.com>
2024-08-13 08:02:01 -0500

    More progress

--- docs/style.js
+++ docs/style.js
@@ -5,25 +5,22 @@ function scrollToTop () {
 
 
 function changeDisplayedExperience(event) {
-    const buttons = document.querySelectorAll("#experience button");
-    const descriptions = document.querySelectorAll("#experience div[id]");
+    const buttons = document.querySelectorAll("#exp-btns button");
+    const descriptions = document.querySelectorAll("#exp-desc div");
+    const clickedButton = event.target.textContent.trim().toLowerCase();
+    
+    buttons.forEach(button => {
+        button.style.backgroundColor = "";
+    });
+    descriptions.forEach(description => description.style.display = "none");
 
-    if (event.target.tagName.toLowerCase() === 'button') {
-        const buttonName = event.target.textContent.trim().toLowerCase();
-        console.log(`Button clicked: ${buttonName}`);
-        buttons.forEach(button => {
-            button.style.backgroundColor = "";
-        });
-        descriptions.forEach(description => description.style.display = "none");
-
-        document.getElementById(buttonName).style.display = "block";
-        event.target.style.backgroundColor = "#a35bff";
-    }
+    document.getElementById(clickedButton).style.display = "block";
+    event.target.style.backgroundColor = "#a35bff";
 }
 
 
 window.onload = () => {
     document.querySelector("#initial-click").style.backgroundColor = "#a35bff";
-    document.querySelector("#experience").addEventListener("click", (event) => changeDisplayedExperience(event));
+    document.querySelector("#exp-btns").addEventListener("click", (event) => changeDisplayedExperience(event));
     document.querySelector("#logo").addEventListener("click", () => scrollToTop());
 }
